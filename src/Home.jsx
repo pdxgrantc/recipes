685ec0bf0ae71d1ae958ef6819bc7c9f217e3c0c
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2024-01-29 14:35:09 -0800

    Add custom theme configurations to tailwind.config.js

---
+++ src/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-full min-h-screen bg-bg text h-full">
+      <div className="h-screen flex flex-nowrap bg-accent_bg"></div>
+    </div>
+  );
+}
